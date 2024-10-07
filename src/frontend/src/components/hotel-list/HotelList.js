a93311be20c0ca0dbbc0ffa75242fc261b8049bb
Ilya Kochetkov <ilya-ko@bk.ru>
2024-10-08 01:03:32 +0400

    added hotel list component

--- src/frontend/src/components/hotel-list/HotelList.js
+++ src/frontend/src/components/hotel-list/HotelList.js
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function HotelList() {
+  const [hotels, setHotels] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5292/api/hotels/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Backend is not responding");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setHotels(data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
-      Hotel list works!
-      <ul></ul>
+      <h1>Hotel List</h1>
+      <ul>
+        {hotels.map((hotel) => (
+          <li key={hotel.id}>{hotel.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
