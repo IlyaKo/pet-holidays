acec6939d306efbdb4623bbd057cda436af77407
Willy <desann@list.ru>
2024-10-21 12:47:20 +0300

    Добавлена валидация при приеме запросов Порт 5433 по умолчанию

--- src/backend/LosTomates.PetHolidays.WebApi/Extensions/ServiceCollectionExtensions.cs
+++ src/backend/LosTomates.PetHolidays.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using LosTomates.PetHolidays.DataAccess;
 using LosTomates.PetHolidays.DataAccess.DataSeed;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 
 namespace LosTomates.PetHolidays.WebApi.Extensions;
 
@@ -22,19 +23,25 @@ internal static class ServiceCollectionExtensions
     internal static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IHotelService, HotelService>();
-
         return services;
     }
 
     internal static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("CoreDb");
+        string? connectionString = configuration.GetConnectionString("CoreDb");
         if (string.IsNullOrWhiteSpace(connectionString))
             throw new ApplicationException("An environment variable named ConnectionStrings__CoreDb is not set");
 
         services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
         services.AddTransient<SeedService>();
 
+        return services;
+    }
+
+    internal static IServiceCollection AddFluentValidation(this IServiceCollection services)
+    {
+        //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddSingleton<IValidator<HotelEditDto>,  HotelEditDtoValidator>();
         return services;
     }
 }
---
+++ src/backend/LosTomates.PetHolidays.WebApi/Validations/HotelEditDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace LosTomates.PetHolidays.WebApi;
+using FluentValidation;
+using FluentValidation.Results;
+using LosTomates.PetHolidays.Application.Hotels;
+using LosTomates.PetHolidays.DataAccess;
+
+public class HotelEditDtoValidator : AbstractValidator<HotelEditDto>
+{
+    public HotelEditDtoValidator()
+    {
+        string message = "Ошибка в поле {PropertyName}: значение {PropertyValue}";
+
+        RuleFor(customer => customer.Name).NotNull()
+                                          .MaximumLength(DatabaseConstrains.NameMaxLength)
+                                          .WithMessage(message);
+        RuleFor(customer => customer.Description).MaximumLength(DatabaseConstrains.DescriptionMaxLength)
+                                                 .When(v => !string.IsNullOrEmpty(v.Description))
+                                                 .WithMessage(message);
+    }
+
+    public override ValidationResult Validate(ValidationContext<HotelEditDto> context)
+    {
+        ValidationResult result = base.Validate(context);
+        
+        foreach(ValidationFailure? error in result.Errors)
+            Console.WriteLine(error.ErrorMessage);
+
+        return result;
+    }
+}
