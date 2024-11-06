ad911bc48c54e4aff792a4b5521e77f2a9de7fa7
Pavel <sps1079@gmail.com>
2024-11-06 23:20:05 +0300

    Revert "rename class to className"
    This reverts commit 8a45bea3ebb9106e89a25c74bad6ca754e6f47b2.

--- src/frontend/src/components/hotel-list/UpdateHotel.js
+++ src/frontend/src/components/hotel-list/UpdateHotel.js
@@ -20,35 +20,35 @@ function UpdateHotel({ hotel, onUpdate }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="box">
-      <h2 className="title is-4">Update Hotel</h2>
+    <form onSubmit={handleSubmit} class="box">
+      <h2 class="title is-4">Update Hotel</h2>
 
-      <div className="field">
-        <label className="label"> Hotel Name </label>
-        <div className="control">
-          <input className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Enter hotel name" autoComplete="off" />
+      <div class="field">
+        <label class="label"> Hotel Name </label>
+        <div class="control">
+          <input class="input" type="text" value={name} onChange={(e) => setName(e.target.value)} required placeholder="Enter hotel name" autoComplete="off" />
         </div>
       </div>
 
-      <div className="field">
-        <label className="label"> Hotel Description </label>
-        <div className="control">
-          <textarea className="textarea" value={description} onChange={(e) => setDescription(e.target.value)} required placeholder="Enter hotel description" autoComplete="off" />
+      <div class="field">
+        <label class="label"> Hotel Description </label>
+        <div class="control">
+          <textarea class="textarea" value={description} onChange={(e) => setDescription(e.target.value)} required placeholder="Enter hotel description" autoComplete="off" />
         </div>
       </div>
 
-      <div className="field">
-        <div className="control">
-          <label className="checkbox">
+      <div class="field">
+        <div class="control">
+          <label class="checkbox">
             <input type="checkbox" checked={isActive} onChange={(e) => setIsActive(e.target.checked)} />
             Is Active
           </label>
         </div>
       </div>
 
-      <div className="field">
-        <div className="control">
-          <button className="button is-primary">Update Hotel</button>
+      <div class="field">
+        <div class="control">
+          <button class="button is-primary">Update Hotel</button>
         </div>
       </div>
     </form>
