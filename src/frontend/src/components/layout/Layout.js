13471cf289daaae6c7e08e3159c91a1dbe813ea5
Pavel <sps1079@gmail.com>
2024-11-06 23:28:19 +0300

    rename layout-1.js to Layout.js

--- src/frontend/src/components/layout/Layout.js
+++ src/frontend/src/components/layout/Layout.js
@@ -3,17 +3,17 @@ import { Link, Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <nav class="navbar">
-        <div class="navbar-brand ">
-          <Link to="/" class="title m-2">
+      <nav className="navbar">
+        <div className="navbar-brand ">
+          <Link to="/" className="title m-2">
             Pet Holidays
           </Link>
         </div>
-        <div class="navbar-start">
-          <Link class="navbar-item" to="/hotels">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/hotels">
             Hotels
           </Link>
-          <Link class="navbar-item" to="/about">
+          <Link className="navbar-item" to="/about">
             About
           </Link>
         </div>
