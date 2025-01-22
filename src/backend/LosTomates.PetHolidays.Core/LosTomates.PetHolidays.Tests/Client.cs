b2a4b515b2a4b6ec375ba2adbf92cacef5e6f115
Pavel <sps1079@gmail.com>
2025-01-22 16:39:44 +0300

    Update project structure

---
+++ src/backend/LosTomates.PetHolidays.Core/LosTomates.PetHolidays.Tests/Client.cs
@@ -0,0 +1,19 @@
+﻿namespace LosTomates.PetHolidays.Tests;
+
+public class Client
+{
+    private HttpClient httpClient;
+
+    public Client()
+    {
+        httpClient = new HttpClient();
+    }
+
+    public void SetOrUpdateTestId()
+    {
+        httpClient.DefaultRequestHeaders.Remove(TestContants.RequestId);
+        httpClient.DefaultRequestHeaders.Remove(TestContants.TestId);
+        httpClient.DefaultRequestHeaders.Add(TestContants.RequestId, Guid.NewGuid().ToString());
+        httpClient.DefaultRequestHeaders.Add(TestContants.TestId, RequestContext.GetTestId());
+    }
+}
