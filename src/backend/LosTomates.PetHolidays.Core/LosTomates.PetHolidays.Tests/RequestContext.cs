9d8c98bd0ae338960f5f9b2fa4f22f6d8597ee4c
Pavel <sps1079@gmail.com>
2025-01-22 16:39:44 +0300

    Update project structure

---
+++ src/backend/LosTomates.PetHolidays.Core/LosTomates.PetHolidays.Tests/RequestContext.cs
@@ -0,0 +1,19 @@
+﻿namespace LosTomates.PetHolidays.Tests;
+
+public class RequestContext
+{
+    private readonly static AsyncLocal<string> TestId = new AsyncLocal<string>();
+
+    public static void SetTestId(string testId)
+    {
+        if(string.IsNullOrWhiteSpace(testId))
+            throw new ArgumentException($"недопустимое значение параметра {nameof(testId)} {testId}");
+
+        if(!string.IsNullOrWhiteSpace(TestId.Value))
+            throw new InvalidOperationException("Значение параметра уже присвоено");
+
+        TestId.Value = testId;
+    }
+
+    public static string GetTestId() => TestId.Value;
+}
