c8916f8faaabda121bbdf141f9c238436ff8a2e8
Pavel <sps1079@gmail.com>
2025-03-09 14:42:30 +0300

    Update PetService

--- src/backend/LosTomates.PetHolidays.Core/LosTomates.PetHolidays.Core.WebApi/Endpoints/PetEndpoints.cs
+++ src/backend/LosTomates.PetHolidays.Core/LosTomates.PetHolidays.Core.WebApi/Endpoints/PetEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using LosTomates.PetHolidays.Core.Application.Pets;
+using LosTomates.PetHolidays.Core.Application.Users;
 
 namespace LosTomates.PetHolidays.Core.WebApi.Endpoints;
 
@@ -10,19 +11,49 @@ public static class PetEndpoints
                           .WithTags("Pets management")
                           .WithOpenApi();
 
-        mapGroup.MapGet(string.Empty, async (IPetService service, string userId) => await service.GetByUserId(userId))
-                .WithSummary("Get user's pets");
+        mapGroup.MapGet(string.Empty, async (IPetService service, ICurrentUserProvider currentUserProvider) =>
+        {
+            var userId = currentUserProvider.GetUserId();
+            var pets = await service.GetByUserId(userId);
+            return Results.Ok(pets);
+        })
+        .WithSummary("Get user's pets")
+        .RequireAuthorization();
 
-        mapGroup.MapGet("{petId}", async (IPetService service, int petId) => await service.GetById(petId))
-                .WithSummary("Get pet by id");
+        mapGroup.MapGet("{petId}", async (IPetService service, ICurrentUserProvider currentUserProvider, int petId) =>
+        {
+            var userId = currentUserProvider.GetUserId();
+            var pet = await service.GetById(petId, userId);
+            return Results.Ok(pet);
+        })
+       .WithSummary("Get pet by id")
+       .RequireAuthorization();
 
-        mapGroup.MapPost(string.Empty, async (IPetService service, string userId, PetEditDto dto) => await service.Create(userId,dto))
-                .WithSummary("Create new pet for user");
+        mapGroup.MapPost(string.Empty, async (IPetService service, ICurrentUserProvider currentUserProvider, PetEditDto dto) =>
+        {
+            var userId = currentUserProvider.GetUserId();
+            var petId = await service.Create(userId, dto);
+            return Results.Created($"/api/pets/{petId}", null);
+        })
+        .WithSummary("Create new pet for user")
+        .RequireAuthorization();
 
-        mapGroup.MapPut("{petId}", async (IPetService service, int petId, PetEditDto dto) => await service.Update(petId, dto))
-                .WithSummary("Update pet");
+        mapGroup.MapPut("{petId}", async (IPetService service, ICurrentUserProvider currentUserProvider, int petId, PetEditDto dto) =>
+        {
+            var userId = currentUserProvider.GetUserId(); 
+            await service.Update(petId, userId, dto);
+            return Results.NoContent();
+        })
+        .WithSummary("Update pet")
+        .RequireAuthorization();
 
-        mapGroup.MapDelete("{petId}", async (IPetService service, int petId) => await service.Delete(petId))
-                .WithSummary("Delete pet");
+        mapGroup.MapDelete("{petId}", async (IPetService service, ICurrentUserProvider currentUserProvider, int petId) =>
+        {
+            var userId = currentUserProvider.GetUserId(); 
+            await service.Delete(petId, userId);
+            return Results.NoContent();
+        })
+        .WithSummary("Delete pet")
+        .RequireAuthorization();
     }
 }
