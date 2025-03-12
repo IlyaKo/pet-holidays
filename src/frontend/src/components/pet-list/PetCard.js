ea463e7d85dba993d06efe7624ec6e5cc8be1f52
Pavel <sps1079@gmail.com>
2025-03-13 00:01:00 +0300

    Update PetCard.js

--- src/frontend/src/components/pet-list/PetCard.js
+++ src/frontend/src/components/pet-list/PetCard.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaEdit } from 'react-icons/fa';
 
 const DEFAULT_PET_PHOTO = "https://img.freepik.com/premium-psd/contact-icon-illustration-isolated_23-2151903357.jpg?w=740";
 
-function PetCard({ pet, onDelete }) {
+function PetCard({ pet, onDelete, onEdit }) {
   return (
-    <div>
-      <div className="w-24 h-24 rounded-full overflow-hidden flex-shrink-0">
-        <img
-          src={pet.photo || DEFAULT_PET_PHOTO}
-          alt={pet.name}
-          className="w-full h-full object-contain"
-        />
-      </div>
-
-      <div>
-        <h3 className="text-lg font-semibold text-center">{pet.name}</h3>
-        <p className="text-gray-600 text-center">{pet.petType.name}</p>
-      </div>
-
-      <div className="flex mt-2">
+    <div className="bg-white shadow-lg rounded-lg p-4 hover:scale-105 transition-transform">
+      <img
+        src={pet.photo || DEFAULT_PET_PHOTO}
+        alt={pet.name}
+        className="rounded-lg w-full h-40 object-cover"
+      />
+      <h3 className="text-xl font-bold mt-2 text-center">Name: {pet.name}</h3>
+      <p className="text-gray-600 text-center">Type: {pet.petType.name}</p>
+      <p className="text-sm text-center">Age: {pet.age} 4 years</p>
+      <p className="text-sm text-center">Weight: {pet.weight} 5 kg</p>
+      <p className="text-sm text-center">Порода: {pet.breed || "Не указано"}</p>
+      
+      <div className="flex justify-between mt-3">
         <button
           onClick={() => onDelete(pet.id)}
-          className="bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-md flex items-center"
+          className="px-3"
         >
           <FaTrash className="mr-2" /> Delete
         </button>
