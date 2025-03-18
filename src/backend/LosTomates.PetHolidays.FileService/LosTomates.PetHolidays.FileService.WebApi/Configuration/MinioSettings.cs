2b184651895c28d627a4634b0160f2c6403aa06d
Pavel <sps1079@gmail.com>
2025-03-18 18:13:51 +0300

    Add PublicUrl to MinioSettings

--- src/backend/LosTomates.PetHolidays.FileService/LosTomates.PetHolidays.FileService.WebApi/Configuration/MinioSettings.cs
+++ src/backend/LosTomates.PetHolidays.FileService/LosTomates.PetHolidays.FileService.WebApi/Configuration/MinioSettings.cs
@@ -3,6 +3,7 @@
 public class MinioSettings
 {
     public string Endpoint { get; set; } = string.Empty;
+    public string PublicUrl { get; set; } = string.Empty;
     public string AccessKey { get; set; } = string.Empty;
     public string SecretKey { get; set; } = string.Empty;
 }
--- src/backend/LosTomates.PetHolidays.FileService/LosTomates.PetHolidays.FileService.WebApi/Services/MinioFileStorage.cs
+++ src/backend/LosTomates.PetHolidays.FileService/LosTomates.PetHolidays.FileService.WebApi/Services/MinioFileStorage.cs
@@ -10,11 +10,13 @@ public class MinioFileStorage : IFileStorage
 {
     private readonly IMinioClient _minioClient;
     private readonly string _baseUrl;
+    private readonly string _publicUrl;
 
     public MinioFileStorage(IOptions<MinioSettings> options, IMinioClient minioClient)
     {
         _minioClient = minioClient ?? throw new ArgumentNullException(nameof(minioClient));
         _baseUrl = options.Value.Endpoint ?? throw new ArgumentException("BaseUrl must not be null", nameof(options));
+        _publicUrl = options.Value.PublicUrl ?? throw new ArgumentException("PublicUrl must not be null", nameof(options));
     }
 
     public async Task<string> UploadFileAsync(string objectName, Stream stream, string bucketName)
@@ -35,7 +37,7 @@ public class MinioFileStorage : IFileStorage
                 .WithObjectSize(stream.Length)
         );
 
-        return $"{_baseUrl}/{bucketName}/{objectName}";
+        return $"{_publicUrl}/{bucketName}/{objectName}";
     }
 
     public async Task<Stream> DownloadFileAsync(string objectName, string bucketName)
@@ -59,17 +61,7 @@ public class MinioFileStorage : IFileStorage
             .WithObject(objectName);
         await _minioClient.StatObjectAsync(statArgs);
 
-        return $"{_baseUrl}/{bucketName}/{objectName}";
-
-        //var statArgs = new StatObjectArgs()
-        //.WithBucket(bucketName)
-        //.WithObject(objectName);
-        //await _minioClient.StatObjectAsync(statArgs);
-
-        //return await _minioClient.PresignedGetObjectAsync(new PresignedGetObjectArgs()
-        //    .WithBucket(bucketName)
-        //    .WithObject(objectName)
-        //    .WithExpiry(3600)); // 1 час
+        return $"{_publicUrl}/{bucketName}/{objectName}";
     }
 
     public async Task DeleteFilesAsync(List<string> objectNames, string bucketName)
